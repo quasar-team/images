2b55a29cca0fb4d737397a24a5112c78f260acbb
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-02-05 11:24:06 +0100

    .

--- w2025/scripts/Common.ps1
+++ w2025/scripts/Common.ps1
@@ -14,12 +14,38 @@ function Invoke-Checked {
 }
 
 function Get-VsDevCmdPath {
+    $programFilesX86 = [Environment]::GetEnvironmentVariable("ProgramFiles(x86)")
+    if ([string]::IsNullOrWhiteSpace($programFilesX86)) {
+        $programFilesX86 = "C:\Program Files (x86)"
+    }
+    $programFiles = [Environment]::GetEnvironmentVariable("ProgramFiles")
+    if ([string]::IsNullOrWhiteSpace($programFiles)) {
+        $programFiles = "C:\Program Files"
+    }
+
     $candidates = @(
-        "$env:ProgramFiles\Microsoft Visual Studio\2026\BuildTools\Common7\Tools\VsDevCmd.bat",
-        "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2026\BuildTools\Common7\Tools\VsDevCmd.bat",
-        "$env:ProgramFiles\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat",
-        "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat"
+        "$programFiles\Microsoft Visual Studio\2026\BuildTools\Common7\Tools\VsDevCmd.bat",
+        "$programFilesX86\Microsoft Visual Studio\2026\BuildTools\Common7\Tools\VsDevCmd.bat",
+        "$programFiles\Microsoft Visual Studio\2025\BuildTools\Common7\Tools\VsDevCmd.bat",
+        "$programFilesX86\Microsoft Visual Studio\2025\BuildTools\Common7\Tools\VsDevCmd.bat",
+        "$programFiles\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat",
+        "$programFilesX86\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat",
+        "$programFiles\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat",
+        "$programFilesX86\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat",
+        "$programFiles\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat",
+        "$programFilesX86\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
     )
+
+    $vswherePath = "$programFilesX86\Microsoft Visual Studio\Installer\vswhere.exe"
+    if (Test-Path $vswherePath) {
+        $installPath = & $vswherePath -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+        if ($LASTEXITCODE -eq 0 -and -not [string]::IsNullOrWhiteSpace($installPath)) {
+            $candidates = @(
+                "$installPath\Common7\Tools\VsDevCmd.bat",
+                "$installPath\VC\Auxiliary\Build\vcvars64.bat"
+            ) + $candidates
+        }
+    }
 
     foreach ($path in $candidates) {
         if (Test-Path $path) {
@@ -36,7 +62,12 @@ function Invoke-VsDevShellCommand {
     )
 
     $vsDevCmd = Get-VsDevCmdPath
-    $fullCommand = ('"{0}" -arch=x64 -host_arch=x64 && {1}' -f $vsDevCmd, $Command)
+    $isVcVars = $vsDevCmd.EndsWith("\vcvars64.bat", [System.StringComparison]::OrdinalIgnoreCase)
+    if ($isVcVars) {
+        $fullCommand = ('"{0}" && {1}' -f $vsDevCmd, $Command)
+    } else {
+        $fullCommand = ('"{0}" -arch=x64 -host_arch=x64 && {1}' -f $vsDevCmd, $Command)
+    }
 
     cmd.exe /S /C $fullCommand
     if ($LASTEXITCODE -ne 0) {
