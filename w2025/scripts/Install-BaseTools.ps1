a60e90665f9b6d4c112338d76c2b0a045d3c3e19
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-02-20 21:13:24 +0100

    [skip ci] Add Windows deps

--- w2025/scripts/Install-BaseTools.ps1
+++ w2025/scripts/Install-BaseTools.ps1
@@ -8,7 +8,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 Write-Host "Installing base tools"
-Invoke-Checked choco @('install', '-y', '--no-progress', 'powershell-core', 'python', 'git', 'git-lfs.install', 'nano', '7zip', 'wixtoolset')
+Invoke-Checked choco @('install', '-y', '--no-progress', 'powershell-core', 'python', 'git', 'git-lfs.install', 'nano', '7zip', 'wixtoolset', 'astyle')
 
 Write-Host "Installing CMake and Ninja"
 Invoke-Checked choco @('install', '-y', '--no-progress', 'cmake', '--installargs', 'ADD_CMAKE_TO_PATH=System')
@@ -20,7 +20,6 @@ Invoke-Checked choco @('install', '-y', '--no-progress', 'visualstudio2026-workl
 
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 refreshenv
-$env:PATH = "C:\Program Files\CMake\bin\;" + $env:PATH
 
 Write-Host "Cleaning Chocolatey cache"
 Invoke-Checked choco @('cache', 'remove')
