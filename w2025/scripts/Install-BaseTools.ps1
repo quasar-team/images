aa9df051fc4a68c6a2ce8305bd8a111afaed5c4e
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-02-23 19:22:20 +0100

    Update pip install commands to use 'pygit2' instead of 'pygit'

--- w2025/scripts/Install-BaseTools.ps1
+++ w2025/scripts/Install-BaseTools.ps1
@@ -26,7 +26,7 @@ Invoke-Checked choco @('cache', 'remove')
 
 Write-Host "Installing Python packages"
 Invoke-Checked python @('-m', 'pip', 'install', '--upgrade', 'pip')
-Invoke-Checked python @('-m', 'pip', 'install', '--no-cache-dir', 'pybind11', 'pytest', 'colorama', 'jinja2', 'lxml', 'pygit')
+Invoke-Checked python @('-m', 'pip', 'install', '--no-cache-dir', 'pybind11', 'pytest', 'colorama', 'jinja2', 'lxml', 'pygit2')
 
 $osCaption = (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
 Add-IssueSection "OS: $osCaption"
