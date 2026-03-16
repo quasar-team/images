0755070524e01d3870ea414c48385928c78ef3ab
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-03-16 10:35:25 +0100

    Enhance build scripts for AlmaLinux 10: add support for position-independent code and improve Net-SNMP configuration

--- alma-10/build-boost.sh
+++ alma-10/build-boost.sh
@@ -17,6 +17,7 @@ curl -fsSL "${BOOST_URL}" \
 pushd /tmp/boost-src
 ./bootstrap.sh
 ./b2 --quiet -d0 -j"$(nproc)" link=static runtime-link=static variant=release threading=multi \
+  cflags=-fPIC cxxflags=-fPIC \
   --prefix="${BOOST_HOME}" install
 popd
 
